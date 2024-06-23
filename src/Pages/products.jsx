3d13aab2a98693c228d0e28396395ca3fd24e28d
vineas <alvienasyandika@gmail.com>
2024-06-23 10:45:28 +0700

    add useState cart

--- src/Pages/products.jsx
+++ src/Pages/products.jsx
@@ -1,14 +1,13 @@
 /* eslint-disable react/jsx-key */
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import CardProduct from "../components/Fragments/CardProduct";
 import Button from "../components/Elements/Button/Index";
-import Counter from "../components/Fragments/Counter";
 
 const products = [
   {
     id: 1,
     name: "Sepatu Baru",
-    price: "Rp. 100.000",
+    price: 100000,
     image: "/img/shoes-1.jpg",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus soluta labore natus adipisci temporibus omnis, voluptatum mollitia aut ut obcaecati?",
@@ -16,37 +15,112 @@ const products = [
   {
     id: 2,
     name: "Sepatu Lama",
-    price: "Rp. 300.000",
+    price: 300000,
+    image: "/img/shoes-1.jpg",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  },
+  {
+    id: 3,
+    name: "Sepatu Bagus",
+    price: 90000,
     image: "/img/shoes-1.jpg",
     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
   },
 ];
 const ProductsPage = () => {
   const email = localStorage.getItem("email");
+  const [cart, setCart] = useState([
+    {
+      id: 1,
+      qty: 1,
+    },
+  ]);
   const handleLogout = () => {
     localStorage.removeItem("email");
     localStorage.removeItem("password");
     window.location.href = "/login";
-  }
+  };
+
+  const handleAddToCart = (id) => {
+    if (cart.find((item) => item.id === id)) {
+      setCart(
+        cart.map((item) =>
+          item.id === id ? { ...item, qty: item.qty + 1 } : item
+        )
+      );
+    } else {
+      setCart([...cart, { id, qty: 1 }]);
+    }
+  };
   return (
     <Fragment>
       <div className="flex justify-end h-20 bg-blue-600 text-white items-center px-10">
-        {email}
-        <Button className="ml-5" onClick={handleLogout}>Logout</Button>
+        <div className="mr-5">{email}</div>
+        <Button onClick={handleLogout}>
+          Logout
+        </Button>
       </div>
       <div className="flex justify-center py-5">
-        {products.map((product) => (
-          <CardProduct key={product.id}>
-            <CardProduct.Header image={product.image} />
-            <CardProduct.Body name={product.name}>
-              {product.description}
-            </CardProduct.Body>
-            <CardProduct.Footer price={product.price} />
-          </CardProduct>
-        ))}
-      </div>
-      <div className="flex w-100 justify-center">
-        <Counter></Counter>
+        <div className="w-4/6 flex flex-wrap">
+          {products.map((product) => (
+            <CardProduct key={product.id}>
+              <CardProduct.Header image={product.image} />
+              <CardProduct.Body name={product.name}>
+                {product.description}
+              </CardProduct.Body>
+              <CardProduct.Footer
+                price={product.price}
+                id={product.id}
+                handleAddToCart={handleAddToCart}
+              />
+            </CardProduct>
+          ))}
+        </div>
+        <div className="w-2/6">
+          <h1 className="text-3xl font-bold text-blue-600 ml-5 mb-2">Cart</h1>
+          {/* <ul>
+            {cart.map((item) => (
+              <li key={item}>{item.id}</li>
+            ))}
+          </ul> */}
+          <table className="text-left table-auto border-separate border-spacing-x-5">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Price</th>
+                <th>Quantity</th>
+                <th>Total</th>
+              </tr>
+            </thead>
+            <tbody>
+              {cart.map((item) => {
+                const product = products.find(
+                  (product) => product.id === item.id
+                );
+                return (
+                  <tr key={item.id}>
+                    <td>{product.name}</td>
+                    <td>
+                      Rp{" "}
+                      {product.price.toLocaleString("id-ID", {
+                        styles: "currency",
+                        currency: "IDR",
+                      })}
+                    </td>
+                    <td>{item.qty}</td>
+                    <td>
+                      Rp{" "}
+                      {(item.qty * product.price).toLocaleString("id-ID", {
+                        styles: "currency",
+                        currency: "IDR",
+                      })}
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
       </div>
     </Fragment>
   );
--- src/components/Elements/Button/Index.jsx
+++ src/components/Elements/Button/Index.jsx
@@ -5,7 +5,7 @@ const Button = (props) => {
       <button
         className={`h-10 px-6 font-semibold rounded-md ${classname} text-white`}
         type={type}
-        onClick={() => onClick()}
+        onClick={onClick}
       >
         {children}
       </button>
